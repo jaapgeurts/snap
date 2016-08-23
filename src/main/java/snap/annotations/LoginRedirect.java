afebc8b980807503e43b40672d34b4d32bf5b977
Jaap Geurts <jaapg@gmx.net>
2016-08-23 10:18:16 +0700

    Added comments

--- src/main/java/snap/annotations/LoginRedirect.java
+++ src/main/java/snap/annotations/LoginRedirect.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Put this annotation on a controller method to enable/disable redirection when
+ * a visitor is not authenticated
+ *
+ * @author Jaap Geurts
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface LoginRedirect
--- src/main/java/snap/annotations/LoginRequired.java
+++ src/main/java/snap/annotations/LoginRequired.java
@@ -5,6 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to make sure a user is logged in. A user is logged in
+ * when
+ *
+ * <pre>
+ * context.getAuthenticatedUser() != null
+ * </pre>
+ *
+ * This annotation can be placed on a controller method or on a controller
+ * class. If put on a controller class all controller methods in that class
+ * require an authenticated user. When put on a class you can mark a method
+ * with @IgnoreLoginRequired to make an exception
+ *
+ * @author Jaap Geurts
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = { ElementType.METHOD, ElementType.TYPE })
 public @interface LoginRequired
